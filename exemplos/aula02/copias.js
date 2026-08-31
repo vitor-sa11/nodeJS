1f7c361327d42140a6c2ab54ee6ee7c376d7a248
seila32 <vitoroliveiradesa9@gmail.com>
2026-08-31 14:11:58 -0300

    Add Atividade 02: Enhance logging for original and updated inventory items

--- exemplos/aula02/copias.js
+++ exemplos/aula02/copias.js
@@ -4,3 +4,8 @@ const original = {
     estoque: 5
 }
 const atualizado = {...original, estoque:8};
+
+console.log({original, atualizado});
+
+const {nome, estoque} = atualizado;
+console.log(`${nome} possui ${estoque} unidade(s)`);
